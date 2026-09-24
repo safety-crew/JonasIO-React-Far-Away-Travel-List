cd4f9221ce1366db28109a22c4de3c1929c541a8
Aman Abuelayyan <153456442+safety-crew@users.noreply.github.com>
2026-09-24 15:48:31 +0300

    Add controlled elements of <form> and <select> in Form Comp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,21 +22,38 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [count, setCount] = useState(1);
+
+  if (!description) {
+    return;
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    const newItem = { description, count, packed: false, id: Date.now() };
+
+    setDescription("");
+    setCount(1);
   }
 
   return (
-    <form className="add-form" onSubmit={() => handleSubmit()}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your ✈ trip?</h3>
-      <select>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
+      <select value={count} onChange={(e) => setCount(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((count) => (
+          <option value={count} key={count}>
+            {count}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
